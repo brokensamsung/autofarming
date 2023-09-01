a62e904abb363e8b77b1a66840518bceb8ba394f
brokensamsung <110091682+brokensamsung@users.noreply.github.com>
2023-09-01 13:22:59 +0200

    Create something.lua

---
+++ something.lua
@@ -0,0 +1,20 @@
+while true do
+    local player = game.Players.LocalPlayer
+    local character = player.Character
+    local targetPosition = Vector3.new(32, 219, 142)
+
+    if player and character then
+        local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+
+        if humanoidRootPart then
+            humanoidRootPart.CFrame = CFrame.new(targetPosition)
+            print("Teleported to (32, 219, 142)")
+        else
+            warn("HumanoidRootPart not found in character.")
+        end
+    else
+        warn("Player or character not found.")
+    end
+
+    wait(10)
+end

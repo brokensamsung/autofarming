708f905270b070f8315f63c7a2e1addfa1c6a5bc
brokensamsung <110091682+brokensamsung@users.noreply.github.com>
2023-09-01 14:57:38 +0200

    Update something.lua

--- something.lua
+++ something.lua
@@ -1,20 +1,32 @@
-while true do
-    local player = game.Players.LocalPlayer
-    local character = player.Character
-    local targetPosition = Vector3.new(32, 219, 142)
+local TweenService = game:GetService("TweenService")
+local TeleportService = game:GetService("TeleportService")
+local player = game.Players.LocalPlayer
+local character = player.Character
+local targetPosition = Vector3.new(32, 219, 142)
+local destinationGameId = 7211666966
 
-    if player and character then
-        local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+local tweenInfo = TweenInfo.new(
+    1,
+    Enum.EasingStyle.Linear,
+    Enum.EasingDirection.Out,
+    0,
+    false,
+    0
+)
 
-        if humanoidRootPart then
-            humanoidRootPart.CFrame = CFrame.new(targetPosition)
-            print("Teleported to Win")
-        else
-            warn("HumanoidRootPart not found in character.")
-        end
+if player and character then
+    local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+
+    if humanoidRootPart then
+        local tween = TweenService:Create(humanoidRootPart, tweenInfo, {CFrame = CFrame.new(targetPosition)})
+        tween:Play()
+        tween.Completed:Connect(function()
+            wait(5)
+            TeleportService:Teleport(destinationGameId, player)
+        end)
     else
-        warn("Player or character not found.")
+        warn("HumanoidRootPart not found in character.")
     end
-
-    wait(10)
+else
+    warn("Player or character not found.")
 end

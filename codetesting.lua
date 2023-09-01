6a6042e22e5c30b5919ba9404d37712a1efb9b92
brokensamsung <110091682+brokensamsung@users.noreply.github.com>
2023-09-01 14:46:45 +0200

    Create codetesting.lua

---
+++ codetesting.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/brokensamsung/autofarming/main/test.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/brokensamsung/autofarming/main/something.lua", true))()

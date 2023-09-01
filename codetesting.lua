ab245ba48d6e4f119c6da46569db732b4b84ddfb
brokensamsung <110091682+brokensamsung@users.noreply.github.com>
2023-09-01 14:50:42 +0200

    Update codetesting.lua

--- codetesting.lua
+++ codetesting.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/brokensamsung/autofarming/main/test.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/brokensamsung/autofarming/main/somethingtemp.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/brokensamsung/autofarming/main/something.lua", true))()
